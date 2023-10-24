d9045dc968a936f686c28c7b8a6d39cbda9686c0
ysinghal310 <gangbuster74@gmail.com>
2023-10-24 15:38:40 +0530

    added email key

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_NMHoTLLQ_6pmmsX7Sgw9B9wtNyP9wMtMd");
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
